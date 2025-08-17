a7887473e1eb233d9a87c550cdfc0a0e69a41d89
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-17 00:07:44 -0400

    Refactor user session management: implement destroy action in SessionsController, update application layout with static title and navigation links, and adjust routes for user sessions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: "users/sessions"
-  }
+  # devise_for :users, controllers: {
+  #   sessions: "users/sessions"
+  # }
 
   resources :messages
   resources :chat_rooms
-  # devise_for :users
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
