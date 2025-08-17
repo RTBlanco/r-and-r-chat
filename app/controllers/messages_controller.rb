485ee0ee2efa6d8cdd2cf5b9715d4100f3df09ed
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-17 02:09:35 -0400

    Implement message creation and display in chat rooms; add new message form and update routes for nested resources

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,23 @@
 class MessagesController < ApplicationController
+  before_action :set_chat_room, only: [:create]
+
+  def create
+    @message = current_user.messages.new(message_params.merge(chat_room: @chat_room))
+
+    if @message.save
+      redirect_to chat_room_path(@chat_room), notice: "Message was successfully created."
+    else
+      redirect_to chat_room_path(@chat_room), alert: "Error creating message."
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :chat_room_id, :user_id)
+  end
+
+  def set_chat_room
+    @chat_room = ChatRoom.find(params[:chat_room_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
     registrations: "user/registrations"
   }
 
-  resources :messages
-  resources :chat_rooms
+  # resources :messages
+  # resources :chat_rooms
+
+  resources :chat_rooms do
+    resources :messages
+  end
   # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
