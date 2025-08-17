52191e605a97e6cb03368bdecef7a963cc132e19
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-16 23:10:52 -0400

    Add ChatRoomsController and views for chat room listing and details; update application layout with navigation and flash messages; enhance user registration form with username field

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -1,2 +1,10 @@
 class ChatRoomsController < ApplicationController
+  def index
+    @chat_rooms = ChatRoom.all
+  end
+
+  def show
+    @chat_room = ChatRoom.find(params[:id])
+    @messages = @chat_room.messages.includes(:user)
+  end
 end
