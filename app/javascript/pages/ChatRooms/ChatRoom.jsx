b35c9819e23d235ee17a1e2d6e93eeb33513c2ba
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-19 19:13:45 -0400

    Refactor ChatRoom component: enhance structure by organizing imports and improving message rendering

--- app/javascript/pages/ChatRooms/ChatRoom.jsx
+++ app/javascript/pages/ChatRooms/ChatRoom.jsx
@@ -1,13 +1,22 @@
 
 import { usePage } from "@inertiajs/react";
+import ChatRoomForm from "./ChatRoomForm";
+import ChatRoomMessages from "./messages/ChatRoomMessages";
+import Message from "./messages/Message";
+import { ListGroup, ListGroupItem } from "flowbite-react";
 
 export default function ChatRoom() {
   const { user, chat_room, messages}  = usePage().props;
 
-
+  console.log(messages)
   return (
     <div className="rounded-sm border border-black h-full">
-      
+      <ChatRoomMessages>
+        {messages.map(message => (
+          <Message key={message.id} message={message} />
+        ))}
+      </ChatRoomMessages>
+      <ChatRoomForm />
     </div>
   );
 }
