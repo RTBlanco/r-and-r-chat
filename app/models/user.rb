e082effa66b1c8523da04a61d99f42d9a16971ba
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-20 08:01:17 -0400

    added uniqness

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :user_name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 20 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }
+
 
   has_many :messages, dependent: :destroy
   has_many :chat_rooms, through: :messages
