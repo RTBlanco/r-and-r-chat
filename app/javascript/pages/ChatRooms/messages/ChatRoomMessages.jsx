6b19ff4ebf5d12f2776a402c252152bdc7fe3f02
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-19 20:00:38 -0400

    made border match list

--- app/javascript/pages/ChatRooms/messages/ChatRoomMessages.jsx
+++ app/javascript/pages/ChatRooms/messages/ChatRoomMessages.jsx
@@ -2,7 +2,7 @@ import { ListGroup } from "flowbite-react";
 
 export default function ChatRoomMessages({children}) {
   return (
-    <div className="rounded-sm border border-black h-9/10">
+    <div className="rounded-lg border border-black h-9/10">
       <ListGroup>
         {children}
       </ListGroup>
