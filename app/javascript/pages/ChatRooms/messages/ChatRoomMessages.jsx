ae2d1742b7815d3a10ff4f0de60cc4b5a1277bd6
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-19 20:35:37 -0400

    removed border and created custom listGroup

--- app/javascript/pages/ChatRooms/messages/ChatRoomMessages.jsx
+++ app/javascript/pages/ChatRooms/messages/ChatRoomMessages.jsx
@@ -2,10 +2,12 @@ import { ListGroup } from "flowbite-react";
 
 export default function ChatRoomMessages({children}) {
   return (
-    <div className="rounded-lg border border-black h-9/10">
-      <ListGroup>
+    <div className="rounded-lg h-9/10">
+      {/* <ListGroup> */}
+      <ul className="list-none rounded-lg  bg-white text-left text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white">
         {children}
-      </ListGroup>
+      </ul>
+      {/* </ListGroup> */}
     </div>
   )
 }
