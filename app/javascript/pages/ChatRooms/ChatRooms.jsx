cc4c1526febd8dad8e6232f72e8ef09485f2f362
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-20 02:20:04 -0400

    made rooms look better

--- app/javascript/pages/ChatRooms/ChatRooms.jsx
+++ app/javascript/pages/ChatRooms/ChatRooms.jsx
@@ -6,7 +6,7 @@ const ChatRooms = ({props}) => {
 
   
   return (
-    <div className="grid grid-cols-4 gap-4">
+    <div className="grid h-full w-full md:grid-cols-4 justify-self-center gap-4">
       {chat_rooms.map(room => (
         <ChatRoomCard key={room.id} room={room}/>
       ))}
