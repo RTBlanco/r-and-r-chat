8394e1acc22f5573f3ecd03eef1a6afc4af935ff
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-19 20:00:46 -0400

    made border match list

--- app/javascript/pages/ChatRooms/ChatRoom.jsx
+++ app/javascript/pages/ChatRooms/ChatRoom.jsx
@@ -8,9 +8,10 @@ import { ListGroup, ListGroupItem } from "flowbite-react";
 export default function ChatRoom() {
   const { user, chat_room, messages}  = usePage().props;
 
+  console.log(user)
   console.log(messages)
   return (
-    <div className="rounded-sm border border-black h-full">
+    <div className="rounded-lg border border-black h-full">
       <ChatRoomMessages>
         {messages.map(message => (
           <Message key={message.id} message={message} />
