976086514e83824b70f7c5ef511404807a762a8c
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-19 20:01:59 -0400

    properly displayed everthing

--- app/javascript/pages/ChatRooms/messages/Message.jsx
+++ app/javascript/pages/ChatRooms/messages/Message.jsx
@@ -1,16 +1,25 @@
 import { Avatar, ListGroupItem } from "flowbite-react";
+import { usePage } from "@inertiajs/react";
 
 export default function Message({message}) {
+  const {user} = usePage().props
+
+  function currentUser() {
+    const color = user.id === message.user.id ? "text-green-500" : "text-yellow-500"
+    return `flex items-center ${color}`
+  }
 
   return (
     <ListGroupItem>
-      <Avatar img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="avatar of Jese" rounded />
-      <div className="content">
-        <div className="name-time">
-          RTblanco 
-          <div className="time"> 5:30 PM</div>
+      <div className="flex">
+        <Avatar className="mr-4" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="avatar of Jese" rounded />
+        <div className="flex flex-col items-start">
+          <div className={currentUser()}>
+            {message.user.user_name} 
+            <div className="text-xs text-gray-900 dark:text-gray-400 ml-2"> 5:30 PM</div>
+          </div>
+          {message.content}
         </div>
-        {message.content}
       </div>
     </ListGroupItem>
   )
