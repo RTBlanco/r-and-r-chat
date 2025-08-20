ce26c71c21ff31971eead369fdc015f85c43b372
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-20 06:07:02 -0400

    made own list group

--- app/javascript/pages/ChatRooms/messages/Message.jsx
+++ app/javascript/pages/ChatRooms/messages/Message.jsx
@@ -10,17 +10,20 @@ export default function Message({message}) {
   }
 
   return (
-    <ListGroupItem>
+    // <ListGroupItem className="ark:hover:bg-gray-500">
+    <div className="mb-2 rounded-lg dark:hover:bg-gray-500">
       <div className="flex">
-        <Avatar className="mr-4" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="avatar of Jese" rounded />
-        <div className="flex flex-col items-start">
+        <Avatar className="mr-4 relative" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="avatar of Jese" rounded />
+        <div className="flex flex-col items-start w-9/10">
           <div className={currentUser()}>
             {message.user.user_name} 
             <div className="text-xs text-gray-900 dark:text-gray-400 ml-2"> 5:30 PM</div>
           </div>
-          {message.content}
+          <p>{message.content}</p>
         </div>
       </div>
-    </ListGroupItem>
+    </div>
+    // </ListGroupItem>
   )
 }
+
