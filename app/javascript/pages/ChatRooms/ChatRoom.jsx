8a9528f2d54a90ee5ad98efa237c02be6f094575
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-20 00:21:24 -0400

    added diffrent class name

--- app/javascript/pages/ChatRooms/ChatRoom.jsx
+++ app/javascript/pages/ChatRooms/ChatRoom.jsx
@@ -7,7 +7,7 @@ import Message from "./messages/Message";
 export default function ChatRoom() {
   const { user, messages}  = usePage().props;
   return (
-    <div className="rounded-lg  h-full">
+    <div className="rounded-lg h-9/10">
       <ChatRoomMessages>
         {messages.map(message => (
           <Message key={message.id} message={message} />
