ec970725c311d53eaddd7b890552e528c4f28352
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-20 08:01:06 -0400

    updated registration page

--- app/javascript/pages/users/registrations/New.jsx
+++ app/javascript/pages/users/registrations/New.jsx
@@ -1,28 +1,68 @@
 
-import { Button, Card, Checkbox, Label, TextInput } from "flowbite-react";
+import { Button, Card, Label, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { router, Link } from '@inertiajs/react'
 
 export default function New() {
+  const [values, setValues] = useState({
+    user:{
+      email: '',
+      user_name: '',
+      password: '',
+    }
+  })
+
+  function handleChange(e) {
+    const key = e.target.name
+    const value = e.target.value
+    setValues((values) => ({
+      ...values,
+      user: {
+        ...values.user,
+        [key]: value,
+      },
+    }))
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    router.post('/users/sign_up', values)
+  }
+
   return (
-    <Card className="max-w-sm">
-      <form className="flex flex-col gap-4">
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="email1">Your email</Label>
+    <div className="h-full flex justify-center">
+      <Card className="max-w-sm w-full ">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="email">Your email</Label>
+            </div>
+            <TextInput id="email" name="email" type="email" placeholder="name@rnr.com" required value={values.user.email} onChange={handleChange}/>
           </div>
-          <TextInput id="email1" type="email" placeholder="name@flowbite.com" required />
-        </div>
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="password1">Your password</Label>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="user_name">Your User Name</Label>
+            </div>
+            <TextInput id="user_name" name="user_name" type="text" required value={values.user.user_name} onChange={handleChange}/>
           </div>
-          <TextInput id="password1" type="password" required />
-        </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="password">Your password</Label>
+            </div>
+            <TextInput id="password" name="password" type="password" required value={values.user.password} onChange={handleChange}/>
+          </div>
+          <Button className='mt-3' type="submit">Submit</Button>
+        </form>
+
         <div className="flex items-center gap-2">
-          <Checkbox id="remember" />
-          <Label htmlFor="remember">Remember me</Label>
-        </div>
-        <Button type="submit">Submit</Button>
-      </form>
-    </Card>
+            <Label className="flex">
+              Already have an account?&nbsp;
+              <Link href="/users/sign_in" className="text-cyan-600 hover:underline dark:text-cyan-500">
+                Sign in!
+              </Link>
+            </Label>
+          </div>
+      </Card>
+    </div>
   );
 }
