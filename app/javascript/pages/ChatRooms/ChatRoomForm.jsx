5b4be3c56354deb5304ab3157c40c66efd37e9f6
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-19 22:20:04 -0400

    made it possible to create a new discord

--- app/javascript/pages/ChatRooms/ChatRoomForm.jsx
+++ app/javascript/pages/ChatRooms/ChatRoomForm.jsx
@@ -1,11 +1,39 @@
 import { TextInput, Label } from "flowbite-react"
-import { usePage } from "@inertiajs/react";
+import { usePage, router } from "@inertiajs/react";
+import { useState } from "react";
 
 export default function ChatRoomForm() {
-  const {chat_room}  = usePage().props;
+  const {user, chat_room}  = usePage().props;
+
+  const [message, setMessage ] = useState("")
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      const messageData = {
+        content: message,
+        user_id: user.id,
+      }   
+      router.post(`/chat_rooms/${chat_room.id}/messages`, messageData)
+      event.target.value = ''
+      
+    }
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value
+    setMessage(value)
+  }
+
   return (
     <div className="w-full">
-      <TextInput id="message" type="test" placeholder={`message in ${chat_room.name}`} required />
+      <TextInput 
+        id="message" 
+        type="test" 
+        placeholder={`message in ${chat_room.name}`} 
+        onKeyDown={handleKeyDown}
+        onChange={handleChange}
+        required 
+      />
     </div>
   )
 }
