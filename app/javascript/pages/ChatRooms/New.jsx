f90b2c3bc5af707034c650fe5006e303f6612227
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-20 02:20:22 -0400

    created form for new chat room

--- app/javascript/pages/ChatRooms/New.jsx
+++ app/javascript/pages/ChatRooms/New.jsx
@@ -1,30 +1,35 @@
 
-import { Button, Card, Checkbox, Label, TextInput } from "flowbite-react";
+import { Button, Card, Label, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { router } from "@inertiajs/react";
 
 export default function New() {
+  const [nameState, setNameState] = useState('')
+
+  function handleOnChange(e) {
+    const value = e.target.value
+    setNameState(value)
+  }
+
+  function handleOnSubmit(e) {
+    e.preventDefault()
+    router.post('/chat_rooms', {name: nameState})
+  }
+
   return (
-    <>
-      <Card className="max-w-sm ">
-        <form className="flex flex-col gap-4">
+    <div className="h-full flex justify-center items-center">
+      <Card className="h-1/2 w-full lg:w-1/2">
+        <form className="flex flex-col gap-4" onSubmit={handleOnSubmit}>
           <div>
-            <div className="mb-2 block">
-              <Label htmlFor="email1">Your email</Label>
+            <div className="mb-2 justify-self-center block">
+              <Label htmlFor="name">Chat Room Name</Label>
             </div>
-            <TextInput id="email1" type="email" placeholder="name@flowbite.com" required />
+            <TextInput id="name" type="text" placeholder="name" onChange={handleOnChange}required />
           </div>
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="password1">Your password</Label>
-            </div>
-            <TextInput id="password1" type="password" required />
-          </div>
-          <div className="flex items-center gap-2">
-            <Checkbox id="remember" />
-            <Label htmlFor="remember">Remember me</Label>
-          </div>
+
           <Button type="submit">Submit</Button>
         </form>
       </Card>
-    </>
+    </div>
   );
 }
