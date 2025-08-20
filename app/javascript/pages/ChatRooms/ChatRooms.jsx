81ac3f1bb829c6d7ebff624dee66026212c5fa18
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-20 06:07:49 -0400

    added toggle switches

--- app/javascript/pages/ChatRooms/ChatRooms.jsx
+++ app/javascript/pages/ChatRooms/ChatRooms.jsx
@@ -1,16 +1,27 @@
 import { usePage } from "@inertiajs/react";
 import ChatRoomCard from "./ChatRoomCard";
+import { ToggleSwitch } from "flowbite-react";
+import { useState } from "react";
 
 const ChatRooms = ({props}) => {
   const { user, chat_rooms}  = usePage().props;
 
+  const [switch1, setSwitch1] = useState(false);
+  const [switch2, setSwitch2] = useState(true);
+
   
   return (
-    <div className="grid h-full w-full md:grid-cols-4 justify-self-center gap-4">
-      {chat_rooms.map(room => (
-        <ChatRoomCard key={room.id} room={room}/>
-      ))}
-    </div>
+    <>
+      <div className="mb-2 flex justify-center w-full gap-2" >
+        <ToggleSwitch checked={switch1} label="Created By me" onChange={setSwitch1} />
+        <ToggleSwitch checked={switch2} label="Joined" onChange={setSwitch2} />
+      </div>
+      <div className="grid h-full w-full md:grid-cols-4 justify-self-center gap-4">
+        {chat_rooms.map(room => (
+          <ChatRoomCard key={room.id} room={room}/>
+        ))}
+      </div>
+    </>
   )
 }
 
