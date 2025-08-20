965ac0ad99a880f6d48d155a22b547db1a4c0d68
RTBlanco <ronnytoribio1@hotmail.com>
2025-08-20 07:26:13 -0400

    made it disappear if user logged out

--- app/javascript/pages/Layout.jsx
+++ app/javascript/pages/Layout.jsx
@@ -15,7 +15,7 @@ import {
 
 const navLinks = {
   "Home" : "/",
-  "Chat Room" : "/chat_rooms",
+  "Chat Rooms" : "/chat_rooms",
   "Create Chat Room" : "/chat_rooms/new"
 }
 
@@ -23,41 +23,44 @@ export default function Layout({children}) {
   const { user }  = usePage().props;
   const { url } = usePage()
 
+  console.log(user)
   return (
-    <div className="">
+    <>
       <Navbar fluid>
-        <NavbarBrand href="https://flowbite-react.com">
+        <NavbarBrand href="https://github.com/RTBlanco/r-and-r-chat">
           {/* <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" /> */}
-          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">R & R</span>
         </NavbarBrand>
-        <div className="flex md:order-2">
-          <Dropdown
-            arrowIcon={false}
-            inline
-            label={
-              <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
-            }
-          >
-            <DropdownHeader>
-              {/* <span className="block truncate text-sm font-medium">{user.user_name}</span> */}
-            </DropdownHeader>
-            <DropdownItem>Dashboard</DropdownItem>
-            <DropdownItem>Settings</DropdownItem>
-            <DropdownItem>Earnings</DropdownItem>
-            <DropdownDivider />
-            <DropdownItem>Sign out</DropdownItem>
-          </Dropdown>
-          <NavbarToggle />
-        </div>
-        <NavbarCollapse>
-          {Object.entries(navLinks).map(([key, val], index) => (
-            <NavbarLink key={index} href={val} as={Link} active={url === val}>{key}</NavbarLink>
-          ))}
-        </NavbarCollapse>
+        {!user || (
+          <>
+            <div className="flex md:order-2">
+              <Dropdown
+                arrowIcon={false}
+                inline
+                label={
+                  <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
+                }
+              >
+                <DropdownHeader>
+                  <span className="block truncate text-sm font-medium">{user.user_name}</span>
+                </DropdownHeader>
+                <DropdownItem>Settings</DropdownItem>
+                <DropdownDivider />
+                <DropdownItem as={Link} method='delete' href='/users/sign_out'>Sign out</DropdownItem>
+              </Dropdown>
+              <NavbarToggle />
+            </div>
+            <NavbarCollapse>
+              {Object.entries(navLinks).map(([key, val], index) => (
+                <NavbarLink key={index} href={val} as={Link} active={url === val}>{key}</NavbarLink>
+              ))}
+            </NavbarCollapse>
+          </>
+        )}
       </Navbar>
       <div className="m-6 h-[86vh]">
         {children}
       </div>
-    </div>
+    </>
   );
 }
