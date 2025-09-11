fc736cdeeb01b35493238e14275ddde1942e4331
RTBlanco <ronnytoribio1@hotmail.com>
2025-09-10 23:12:50 -0400

    commented out stuff

--- app/javascript/channels/chat_room_channel.js
+++ app/javascript/channels/chat_room_channel.js
@@ -1,15 +1,25 @@
-import consumer from "./consumer"
+// import consumer from "./consumer"
 
-consumer.subscriptions.create("ChatRoomChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+// consumer.subscriptions.create("ChatRoomChannel", {
+//   // connected() {
+//   //   // Called when the subscription is ready for use on the server
+//   // },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+//   // disconnected() {
+//   //   // Called when the subscription has been terminated by the server
+//   // },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
+//   // received(data) {
+//   //   // Called when there's incoming data on the websocket for this channel
+//   // }
+
+//   connected() {
+//     console.log("Connected to the channel:", this);
+//   },
+//   disconnected() {
+//     console.log("Disconnected");
+//   },
+//   received(data) {
+//     console.log("Received some data:", data);
+//   }
+// });
