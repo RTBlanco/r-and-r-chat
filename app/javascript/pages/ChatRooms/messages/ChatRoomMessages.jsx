4f4fb8b92d24cc2e4fe29e0f9cef39d1ba66c594
RTBlanco <ronnytoribio1@hotmail.com>
2025-11-26 21:22:10 -0500

    made it scroll on the most recent page

--- app/javascript/pages/ChatRooms/messages/ChatRoomMessages.jsx
+++ app/javascript/pages/ChatRooms/messages/ChatRoomMessages.jsx
@@ -2,7 +2,7 @@ import { ListGroup } from "flowbite-react";
 
 export default function ChatRoomMessages({children}) {
   return (
-    <div className="rounded-lg overflow-auto">
+    <div className="rounded-lg overflow-auto flex flex-col-reverse">
       {/* <ListGroup> */}
       <ul className="list-none rounded-lg  bg-white text-left text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-white">
         {children}
