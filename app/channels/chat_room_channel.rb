13b98d70d1ddabc16458b020675eae2270bce69f
RTBlanco <ronnytoribio1@hotmail.com>
2025-11-28 01:04:21 -0500

    removed console.log

--- app/channels/chat_room_channel.rb
+++ app/channels/chat_room_channel.rb
@@ -1,7 +1,7 @@
 class ChatRoomChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-    binding.break
+    # binding.break
     chat_room = ChatRoom.find(params[:chat_room_id])
     stream_for chat_room
   end
