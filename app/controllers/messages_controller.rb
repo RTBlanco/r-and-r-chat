50f992974bb57658ec45bbb45daa8c804ff5cb0e
RTBlanco <ronnytoribio1@hotmail.com>
2025-12-01 19:41:20 -0500

    correclty displaying message data

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,10 +2,20 @@ class MessagesController < ApplicationController
   before_action :set_chat_room, only: [:create]
 
   def create
-    @message = current_user.messages.new(message_params.merge(chat_room: @chat_room))
+    message = current_user.messages.new(message_params.merge(chat_room: @chat_room))
 
-    if @message.save
-      ChatRoomChannel.broadcast_to(@chat_room, @message.content)
+    if message.save
+      @message = {
+        id: message.id,
+        content: message.content,
+        chat_room_id: message.chat_room_id,
+        user: {
+          id: message.user.id,
+          user_name: message.user.user_name,
+          avatar: message.user.avatar.attached? ? url_for(message.user.avatar) : nil
+        }
+      }
+      ChatRoomChannel.broadcast_to(@chat_room, @message)
       redirect_to chat_room_path(@chat_room)
     else
       flash[:failure] = "Error creating message."
