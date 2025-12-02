ba42bfa6991059d809695d574d80bc316d59fcc1
RTBlanco <ronnytoribio1@hotmail.com>
2025-12-01 19:14:29 -0500

    added comment

--- app/channels/chat_room_channel.rb
+++ app/channels/chat_room_channel.rb
@@ -2,6 +2,7 @@ class ChatRoomChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
     # binding.break
+    # stream new stuff here ?
     chat_room = ChatRoom.find(params[:chat_room_id])
     stream_for chat_room
   end
