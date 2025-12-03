e8e88d25fa61d8aca759d6b3b02a0ea8c0d9e889
RTBlanco <ronnytoribio1@hotmail.com>
2025-12-03 14:52:00 -0500

    used to Form Component

--- app/javascript/pages/ChatRooms/ChatRoomForm.jsx
+++ app/javascript/pages/ChatRooms/ChatRoomForm.jsx
@@ -1,38 +1,25 @@
 import { TextInput, Label } from "flowbite-react"
 import { usePage, router } from "@inertiajs/react";
 import { useState } from "react";
+import { Form } from '@inertiajs/react'
 
 export default function ChatRoomForm({className}) {
   const {user, chat_room}  = usePage().props;
 
-  const [message, setMessage ] = useState("")
-
-  const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
-      const messageData = {
-        content: message,
-        user_id: user.id,
-      }   
-      router.post(`/chat_rooms/${chat_room.id}/messages`, messageData)
-      event.target.value = ''
-    }
-  };
-
-  const handleChange = (e) => {
-    const value = e.target.value
-    setMessage(value)
-  }
-
   return (
-    <div className={className}>
+    <Form className={className} 
+      action={`/chat_rooms/${chat_room.id}/messages`} 
+      method="POST"
+      transform={(data) => ({...data, user_id: user.id})}
+      resetOnSuccess
+      >
       <TextInput 
-        id="message" 
+        id="content"
+        name="content" 
         type="text"
         placeholder={`message in ${chat_room.name}`} 
-        onKeyDown={handleKeyDown}
-        onChange={handleChange}
         required
       />
-    </div>
+    </Form>
   )
 }
