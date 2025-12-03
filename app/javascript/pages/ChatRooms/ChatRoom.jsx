99fb7200b8572b3aed4269d69ae9bfe5c70779ec
RTBlanco <ronnytoribio1@hotmail.com>
2025-12-03 18:13:18 -0500

    got it to show one at a time

--- app/javascript/pages/ChatRooms/ChatRoom.jsx
+++ app/javascript/pages/ChatRooms/ChatRoom.jsx
@@ -13,31 +13,44 @@ window.App.cable = ActionCable.createConsumer();
 
 export default function ChatRoom() {
   const {chat_room, messages}  = usePage().props;
-  const [statetMessages, setStateMessages] = useState(messages)
+  const [stateMessages, setStateMessages] = useState(messages)
   
-  App.cable.subscriptions.create({ channel: "ChatRoomChannel", chat_room_id: chat_room.id }, {
-    connected() {
-      console.log("Connected to the channel:", this);
-    },
-    disconnected() {
-      console.log("Disconnected");
-    },
-    received(data) {
-      console.log("Received some data:", data);
-      // console.log(messages) 
-      setStateMessages( m => [...m, data] )
-      console.log(statetMessages)
-    }
-  });
-  
-  
+  useEffect(() => {
+    // setStateMessages(messages)
+    App.cable.subscriptions.create({ channel: "ChatRoomChannel", chat_room_id: chat_room.id }, {
+      connected() {
+        console.log("Connected to the channel:", this);
+      },
+      disconnected() {
+        console.log("Disconnected");
+      },
+      received(data) {
+        console.log("Received some data:", data);
+        setStateMessages(m => [...m, data])
+        // console.log(messages) 
+        // const new_messages = stateMessages.map(message => {
+        //   // console.log(message.id)
+        //   // console.log(data.id)
+        //   if (message.id === data.id) {
+        //     return data
+        //   } else {
+        //     message
+        //   }
+        // })
+        // setStateMessages(new_messages)
+        // // console.log(statetMessages)
+        // console.log(stateMessages)
+      }
+    });
+  },[])
+
    
   return (
     <>
       <h1 className="flex justify-center text-sky-50">{chat_room.name}</h1>
       <div className="rounded-lg flex flex-col justify-between h-full">
         <ChatRoomMessages>
-          {statetMessages.map(message => (
+          {stateMessages.map(message => (
             <Message key={message.id} message={message} />
           ))}
         </ChatRoomMessages>
@@ -45,4 +58,4 @@ export default function ChatRoom() {
       <ChatRoomForm className='fixed bottom-0 left-0 w-full'/>
     </>
   );
-}
+} 
