bf4086dd33e124f55031206ce716669daeb14303
RTBlanco <ronnytoribio1@hotmail.com>
2025-12-03 15:09:22 -0500

    removed instance calling

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,21 +2,22 @@ class MessagesController < ApplicationController
   before_action :set_chat_room, only: [:create]
 
   def create
-    message = current_user.messages.new(message_params.merge(chat_room: @chat_room))
+    # binding.pry
+    new_message = current_user.messages.new(message_params.merge(chat_room: @chat_room))
 
-    if message.save
-      @message = {
-        id: message.id,
-        content: message.content,
-        chat_room_id: message.chat_room_id,
+    if new_message.save
+      message = {
+        id: new_message.id,
+        content: new_message.content,
+        chat_room_id: new_message.chat_room_id,
         user: {
-          id: message.user.id,
-          user_name: message.user.user_name,
-          avatar: message.user.avatar.attached? ? url_for(message.user.avatar) : nil
+          id: new_message.user.id,
+          user_name: new_message.user.user_name,
+          avatar: new_message.user.avatar.attached? ? url_for(new_message.user.avatar) : nil
         }
       }
-      ChatRoomChannel.broadcast_to(@chat_room, @message)
-      # redirect_to chat_room_path(@chat_room) // this makes it reload the page
+      ChatRoomChannel.broadcast_to(@chat_room, message)
+      redirect_to chat_room_path(@chat_room)
     else
       flash[:failure] = "Error creating message."
       redirect_to chat_room_path(@chat_room)
