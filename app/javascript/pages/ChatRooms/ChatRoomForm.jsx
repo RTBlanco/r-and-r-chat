3f6e2ecabdc1418e7464c2712b42b802e89749bd
RTBlanco <ronnytoribio1@hotmail.com>
2025-12-03 18:13:45 -0500

    reverted back to how it was becasue inertia got some issue it need to figure out

--- app/javascript/pages/ChatRooms/ChatRoomForm.jsx
+++ app/javascript/pages/ChatRooms/ChatRoomForm.jsx
@@ -1,26 +1,59 @@
 import { TextInput } from "flowbite-react"
-import { usePage } from "@inertiajs/react";
+import { usePage, router } from "@inertiajs/react";
 import { Form } from '@inertiajs/react'
+import { useState } from "react";
 
 
 export default function ChatRoomForm({className}) {
   const {user, chat_room}  = usePage().props;
 
 
+  // return (
+  //   <Form className={className} 
+  //     action={`/chat_rooms/${chat_room.id}/messages`} 
+  //     method="POST"
+  //     transform={(data) => ({...data, user_id: user.id})}
+  //     resetOnSuccess
+  //     >
+  //     <TextInput 
+  //       id="content"
+  //       name="content" 
+  //       type="text"
+  //       placeholder={`message in ${chat_room.name}`} 
+  //       required
+  //     />
+  //   </Form>
+  // )
+ 
+
+  const [message, setMessage ] = useState("")
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      const messageData = {
+        content: message,
+        user_id: user.id,
+      }   
+      router.post(`/chat_rooms/${chat_room.id}/messages`, messageData, {except: ['chat_room', 'messages']})
+      event.target.value = ''
+    }
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value
+    setMessage(value)
+  }
+
   return (
-    <Form className={className} 
-      action={`/chat_rooms/${chat_room.id}/messages`} 
-      method="POST"
-      transform={(data) => ({...data, user_id: user.id})}
-      resetOnSuccess
-      >
+    <div className={className}>
       <TextInput 
-        id="content"
-        name="content" 
+        id="message" 
         type="text"
         placeholder={`message in ${chat_room.name}`} 
+        onKeyDown={handleKeyDown}
+        onChange={handleChange}
         required
       />
-    </Form>
+    </div>
   )
 }
