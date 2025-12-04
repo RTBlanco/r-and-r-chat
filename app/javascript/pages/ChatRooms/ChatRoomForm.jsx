9332f0d84a93bb2ce4141f73f6cd120cad0e449b
RTBlanco <ronnytoribio1@hotmail.com>
2025-12-03 19:20:06 -0500

    fixed little issue with mobile

--- app/javascript/pages/ChatRooms/ChatRoomForm.jsx
+++ app/javascript/pages/ChatRooms/ChatRoomForm.jsx
@@ -36,6 +36,7 @@ export default function ChatRoomForm({className}) {
       }   
       router.post(`/chat_rooms/${chat_room.id}/messages`, messageData, {except: ['chat_room', 'messages']})
       event.target.value = ''
+      event.target.blur();
     }
   };
 
@@ -53,6 +54,7 @@ export default function ChatRoomForm({className}) {
         onKeyDown={handleKeyDown}
         onChange={handleChange}
         required
+        sizing="lg"
       />
     </div>
   )
