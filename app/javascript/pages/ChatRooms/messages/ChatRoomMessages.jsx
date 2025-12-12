e60ba7a4fedf009a9aabab5ed71cf57ac495c0cf
RTBlanco <ronnytoribio1@hotmail.com>
2025-12-12 06:46:46 -0500

    removed the div

--- app/javascript/pages/ChatRooms/messages/ChatRoomMessages.jsx
+++ app/javascript/pages/ChatRooms/messages/ChatRoomMessages.jsx
@@ -2,12 +2,14 @@ import { ListGroup } from "flowbite-react";
 
 export default function ChatRoomMessages({children}) {
   return (
-    <div className="rounded-lg pb-20 overflow-auto flex flex-col-reverse">
+    // <div className="rounded-lg pb-20 overflow-auto flex flex-col-reverse">
+    <>
       {/* <ListGroup> */}
       <ul className="list-none rounded-lg  bg-white text-left text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-white">
         {children}
       </ul>
-      {/* </ListGroup> */}
-    </div>
+      {/* </ListGroup> */}    
+    </>
+    // </div>
   )
 }
