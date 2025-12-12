5374316761ded6093b8c5376d3bc2ce8872f910b
RTBlanco <ronnytoribio1@hotmail.com>
2025-12-12 06:49:22 -0500

    removed commented code

--- app/javascript/pages/ChatRooms/ChatRoomForm.jsx
+++ app/javascript/pages/ChatRooms/ChatRoomForm.jsx
@@ -1,77 +1,11 @@
 import { TextInput } from "flowbite-react"
-import { usePage, router } from "@inertiajs/react";
+import { usePage } from "@inertiajs/react";
 import { Form } from '@inertiajs/react'
-import { useState } from "react";
 import { useMediaQuery } from 'react-responsive';
 
 export default function ChatRoomForm({className}) {
   const isMobile = useMediaQuery({query: '(max-width: 600px'})
   const {user, chat_room}  = usePage().props;
-
-
-  // return (
-  //   <Form className={className} 
-  //     action={`/chat_rooms/${chat_room.id}/messages`} 
-  //     method="POST"
-  //     transform={(data) => ({...data, user_id: user.id})}
-  //     resetOnSuccess
-  //     >
-  //     <TextInput 
-  //       id="content"
-  //       name="content" 
-  //       type="text"
-  //       placeholder={`message in ${chat_room.name}`} 
-  //       required
-  //     />
-  //   </Form>
-  // )
- 
-
-  const [message, setMessage ] = useState("")
-
-  const handleKeyDown = (event) => {
-    // console.log(event)
-    // if (event.key === 'Enter') {
-    //   const messageData = {
-    //     content: message,
-    //     user_id: user.id,
-    //   }   
-    //   router.post(`/chat_rooms/${chat_room.id}/messages`, messageData, {except: ['chat_room', 'messages']})
-    //   event.target.value = ''
-    // }
-  };
-
-  const handleOnSubmit = (e) => {
-    e.preventDefault()
-    
-    // if (isMobile) {
-      console.log(e)
-      const messageData = {
-        content: message,
-        user_id: user.id,
-      }   
-      router.post(`/chat_rooms/${chat_room.id}/messages`, messageData, {except: ['chat_room', 'messages']})
-    // }
-  }
-
-  const handleChange = (e) => {
-    const value = e.target.value
-    setMessage(value)
-  }
-
-  // return (
-  //   <form className={className} onSubmit={handleOnSubmit}>
-  //     <TextInput 
-  //       id="message" 
-  //       type="text"
-  //       placeholder={`message in ${chat_room.name}`} 
-  //       // onKeyDown={handleKeyDown}
-  //       // onChange={handleChange}
-  //       required
-  //       sizing="lg"
-  //     />
-  //   </form>
-  // )
 
   return (
     <Form className={className} 
