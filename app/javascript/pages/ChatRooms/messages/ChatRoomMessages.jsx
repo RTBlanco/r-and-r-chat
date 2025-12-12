791928000905e3ac7aee5151818393e3e8eda022
RTBlanco <ronnytoribio1@hotmail.com>
2025-12-12 06:49:31 -0500

    removed comments

--- app/javascript/pages/ChatRooms/messages/ChatRoomMessages.jsx
+++ app/javascript/pages/ChatRooms/messages/ChatRoomMessages.jsx
@@ -1,15 +1,10 @@
-import { ListGroup } from "flowbite-react";
 
 export default function ChatRoomMessages({children}) {
   return (
-    // <div className="rounded-lg pb-20 overflow-auto flex flex-col-reverse">
     <>
-      {/* <ListGroup> */}
       <ul className="list-none rounded-lg  bg-white text-left text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-white">
         {children}
-      </ul>
-      {/* </ListGroup> */}    
+      </ul>  
     </>
-    // </div>
   )
 }
